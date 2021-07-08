0d9559e2f42ab2d8ec3784f7656c810a7a45f2a7
Hsy <a553331998@gmail.com>
2021-07-09 00:10:07 +0800

    up

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -3,41 +3,44 @@
     <div class="tags">
       <router-link
         class="tag"
-        :to="`/labels/edit/${tag.id}`"
         v-for="tag in tags"
         :key="tag.id"
+        :to="`/labels/edit/${tag.id}`"
       >
         <span>{{ tag.name }}</span>
         <Icon name="right" />
       </router-link>
     </div>
     <div class="createTag-wrapper">
-      <Button class="createTag" @click="createTag">新建标签</Button>
+      <Button class="createTag" @click.native="createTag">
+        新建标签
+      </Button>
     </div>
   </Layout>
 </template>
 
-<script>
+<script lang="ts">
 import Vue from 'vue'
 import { Component } from 'vue-property-decorator'
-import tagListModel from '../models/tagListModel'
-import Button from '../components/Button.vue'
-
+import tagListModel from '@/models/tagListModel'
+import Button from '@/components/Button.vue'
 tagListModel.fetch()
-@Component
-export default class labels extends Vue {
+@Component({
+  components: { Button },
+})
+export default class Labels extends Vue {
   tags = tagListModel.data
   createTag() {
     const name = window.prompt('请输入标签名')
     if (name) {
       const message = tagListModel.create(name)
       if (message === 'duplicated') {
-        window.alert('标签不能重复')
+        window.alert('标签名重复了')
       } else if (message === 'success') {
         window.alert('新建标签成功')
+      } else {
+        window.alert('标签名不能为空')
       }
-    } else {
-      window.alert('标签名不能为空')
     }
   }
 }
@@ -48,31 +51,27 @@ export default class labels extends Vue {
   background: white;
   font-size: 16px;
   padding-left: 16px;
-  .tag {
-    display: flex;
+  > .tag {
     min-height: 44px;
-    justify-content: space-between;
+    display: flex;
     align-items: center;
+    justify-content: space-between;
     border-bottom: 1px solid #e6e6e6;
     svg {
-      color: #333;
-      margin-right: 16px;
       width: 18px;
       height: 18px;
+      color: #666;
+      margin-right: 16px;
     }
   }
-  .createTag {
-    background: #2355fa;
-    color: white;
-    border-radius: 4px;
-    border: none;
-    height: 40px;
-    padding: 0 16px;
-    &-wrapper {
-      text-align: center;
-      padding: 16px;
-      margin-top: 44-16px;
-    }
+}
+.createTag {
+  background: #008c8c;
+
+  &-wrapper {
+    text-align: center;
+    padding: 16px;
+    margin-top: 44-16px;
   }
 }
 </style>
